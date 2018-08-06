81992966452f14566efd64debc77dcd8a75dce70
Kieron Browne <kbrowne@pivotal.io>
2018-08-06 12:30:01 +0100

    fix(deploy): Setting correct env

--- ci/scripts/cf_deploy.sh
+++ ci/scripts/cf_deploy.sh
@@ -3,4 +3,5 @@ export GOPATH=$PWD/go
 cd $1
 cf api https://api.islington.services-enablement.cf-app.com  --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PASSWORD
+cf target -o interview -s interview
 cf push . --random-route
