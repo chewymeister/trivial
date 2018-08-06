27749a60609e942f99b2f2f42c49a8aa13a9f042
Kieron Browne <kbrowne@pivotal.io>
2018-08-06 12:26:25 +0100

    fix(Deploy): Correct ssl error

--- ci/scripts/cf_deploy.sh
+++ ci/scripts/cf_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GOPATH=$PWD/go
 cd $1
-cf api https://api.islington.services-enablement.cf-app.com
+cf api https://api.islington.services-enablement.cf-app.com  --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PASSWORD
 cf push . --random-route
